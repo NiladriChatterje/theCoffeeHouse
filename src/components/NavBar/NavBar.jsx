2b34bcb45c85b580ade878c1575d40486426a6fe
unknown <radongtxgpu@gmail.com>
2022-09-07 01:16:19 +0530

    third

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './NavBar.css';
 
+const navbar_items =['Home','Menu','Blog','Contact'];
 const NavBar = () => {
   return (
     <div className='navbar'>
-      <div></div>
+      <div>
+        <ul className='navbar_items'>
+        {navbar_items?.map((item,i)=><li key={i}>
+          <div></div>{item}
+        </li>
+       )}
+       </ul>
+      </div>
     </div>
   )
 }
