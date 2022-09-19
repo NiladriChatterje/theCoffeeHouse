e467a096e8f263fc0cf6d04dc4e8e3ee9c7ba4bc
NiladriChatterje <radongtxgpu@gmail.com>
2022-09-19 11:50:40 +0530

    fifth

--- src/calculationScroll.js
+++ src/calculationScroll.js
@@ -27,146 +27,116 @@ const mouseMove = (e) => {
         document.getElementById('p6').style.setProperty('--mouseY',-y*10+"%");
 }
 const mouseEnter = () =>{
-        document.getElementById('p1').style.top='-25%';
-        document.getElementById('p1').style.left='-25%';
+        
+        document.getElementById('p1').style.setProperty('--mouseX','-25%');
+        document.getElementById('p1').style.setProperty('--mouseY','-25%');
 
-        document.getElementById('p2').style.top='-45%';
-        document.getElementById('p2').style.left='30%';
+        document.getElementById('p2').style.setProperty('--mouseX','-45%');
+        document.getElementById('p2').style.setProperty('--mouseY','30%');
 
-        document.getElementById('p3').style.top='-15%';
-        document.getElementById('p3').style.left='80%';
+        document.getElementById('p3').style.setProperty('--mouseX','-15%');
+        document.getElementById('p3').style.setProperty('--mouseY','80%');
 
-        document.getElementById('p4').style.top='40%';
-        document.getElementById('p4').style.left='80%';
+        document.getElementById('p4').style.setProperty('--mouseX','40%');
+        document.getElementById('p4').style.setProperty('--mouseY','80%');
 
-        document.getElementById('p5').style.top='85%';
-        document.getElementById('p5').style.left='20%';
+        document.getElementById('p5').style.setProperty('--mouseX','85%');
+        document.getElementById('p5').style.setProperty('--mouseY','20%');
 
-        document.getElementById('p6').style.top='85%';
-        document.getElementById('p6').style.left='-20%';
+        document.getElementById('p6').style.setProperty('--mouseX','85%');
+        document.getElementById('p6').style.setProperty('--mouseY','-20%');
 }
 
 const mouseLeave = (e) =>{
 
-    document.getElementById('p1').style.top='+25%';
-        document.getElementById('p1').style.left='+25%';
         document.getElementById('p1').style.setProperty('--mouseX','-80%');
         document.getElementById('p1').style.setProperty('--mouseY','-80%');
 
-
-        document.getElementById('p2').style.top='-15%';
-        document.getElementById('p2').style.left='30%';
         document.getElementById('p2').style.setProperty('--mouseX',-50 +"%");
         document.getElementById('p2').style.setProperty('--mouseY',-30+"%");
 
-
-        document.getElementById('p3').style.top='-05%';
-        document.getElementById('p3').style.left='20%';
-        document.getElementById('p3').style.setProperty('--mouseX',30 +"%");
-        document.getElementById('p3').style.setProperty('--mouseY',30+"%");
+        document.getElementById('p3').style.setProperty('--mouseX',-190 +"%");
+        document.getElementById('p3').style.setProperty('--mouseY',70+"%");
       
-       
-        
-        document.getElementById('p4').style.top='35%';
-        document.getElementById('p4').style.left='30%';
-        document.getElementById('p4').style.setProperty('--mouseX',30 +"%");
+        document.getElementById('p4').style.setProperty('--mouseX',-150 +"%");
         document.getElementById('p4').style.setProperty('--mouseY',30+"%");
 
-        document.getElementById('p5').style.top='65%';
-        document.getElementById('p5').style.left='0%';
         document.getElementById('p5').style.setProperty('--mouseX',30 +"%");
-        document.getElementById('p5').style.setProperty('--mouseY',30+"%");
+        document.getElementById('p5').style.setProperty('--mouseY',0+"%");
 
 
-        document.getElementById('p6').style.top='55%';
-        document.getElementById('p6').style.left='-10%';
-        document.getElementById('p6').style.setProperty('--mouseX',30 +"%");
-        document.getElementById('p6').style.setProperty('--mouseY',30+"%");
+       
+        document.getElementById('p6').style.setProperty('--mouseX',0 +"%");
+        document.getElementById('p6').style.setProperty('--mouseY',-130+"%");
 }
 const touchStart = ()=>{
-        document.getElementById('p1').style.top='-35%';
-        document.getElementById('p1').style.left='-35%';
+    document.getElementById('p1').style.setProperty('--mouseX','-35%');
+    document.getElementById('p1').style.setProperty('--mouseY','-35%');
 
-        document.getElementById('p2').style.top='-55%';
-        document.getElementById('p2').style.left='40%';
+    document.getElementById('p2').style.setProperty('--mouseX','-45%');
+    document.getElementById('p2').style.setProperty('--mouseY','30%');
 
-        document.getElementById('p3').style.top='-15%';
-        document.getElementById('p3').style.left='80%';
+    document.getElementById('p3').style.setProperty('--mouseX','-15%');
+    document.getElementById('p3').style.setProperty('--mouseY','80%');
 
-        document.getElementById('p4').style.top='40%';
-        document.getElementById('p4').style.left='80%';
+    document.getElementById('p4').style.setProperty('--mouseX','40%');
+    document.getElementById('p4').style.setProperty('--mouseY','80%');
 
-        document.getElementById('p5').style.top='85%';
-        document.getElementById('p5').style.left='20%';
+    document.getElementById('p5').style.setProperty('--mouseX','85%');
+    document.getElementById('p5').style.setProperty('--mouseY','20%');
 
-        document.getElementById('p6').style.top='85%';
-        document.getElementById('p6').style.left='-20%';
+    document.getElementById('p6').style.setProperty('--mouseX','85%');
+    document.getElementById('p6').style.setProperty('--mouseY','-20%');
 }
 const touchMove = (e)=>{
-    let x = -(e.changedTouches[0].pageX)+(Array.from(document.getElementsByClassName('App'))[0].clientWidth)/2;
-        let y =-(e.changedTouches[0].pageY)+(Array.from(document.getElementsByClassName('App'))[0].clientHeight)/2;
+    let x = -(e.changedTouches[0].clientX)+(Array.from(document.getElementsByClassName('App'))[0].clientWidth)/2;
+        let y =-(e.changedTouches[0].clientY)+(Array.from(document.getElementsByClassName('App'))[0].clientHeight)/2;
         console.log(e);
         
-        document.getElementById('p1').style.setProperty('--mouseX',x*1.2 +"%");
-        document.getElementById('p1').style.setProperty('--mouseY',y*1.2+"%");
+        document.getElementById('p1').style.setProperty('--mouseX',x*2 +"%");
+        document.getElementById('p1').style.setProperty('--mouseY',y*2+"%");
 
         
-        document.getElementById('p2').style.setProperty('--mouseX',-x*1.2 +"%");
-        document.getElementById('p2').style.setProperty('--mouseY',-y*1.2+"%");
+        document.getElementById('p2').style.setProperty('--mouseX',-x*2 +"%");
+        document.getElementById('p2').style.setProperty('--mouseY',-y*2+"%");
 
         
-        document.getElementById('p3').style.setProperty('--mouseX',x*1.2 +"%");
-        document.getElementById('p3').style.setProperty('--mouseY',y*1.2+"%");
+        document.getElementById('p3').style.setProperty('--mouseX',x*2 +"%");
+        document.getElementById('p3').style.setProperty('--mouseY',y*2+"%");
 
         
-        document.getElementById('p4').style.setProperty('--mouseX',-x*1.2 +"%");
-        document.getElementById('p4').style.setProperty('--mouseY',-y*1.2+"%");
+        document.getElementById('p4').style.setProperty('--mouseX',-x*2 +"%");
+        document.getElementById('p4').style.setProperty('--mouseY',-y*2+"%");
       
         
-        document.getElementById('p5').style.setProperty('--mouseX',x*1.2 +"%");
-        document.getElementById('p5').style.setProperty('--mouseY',y*1.2+"%");
+        document.getElementById('p5').style.setProperty('--mouseX',x*2 +"%");
+        document.getElementById('p5').style.setProperty('--mouseY',y*2+"%");
 
         
-        document.getElementById('p6').style.setProperty('--mouseX',-x*1.2 +"%");
-        document.getElementById('p6').style.setProperty('--mouseY',-y*1.2+"%");
+        document.getElementById('p6').style.setProperty('--mouseX',-x*2 +"%");
+        document.getElementById('p6').style.setProperty('--mouseY',-y*2+"%");
 }
 
 
 const touchEnd =() =>{
-    document.getElementById('p1').style.top='+25%';
-        document.getElementById('p1').style.left='+25%';
+   
         document.getElementById('p1').style.setProperty('--mouseX','-80%');
         document.getElementById('p1').style.setProperty('--mouseY','-80%');
-
-
-        document.getElementById('p2').style.top='-15%';
-        document.getElementById('p2').style.left='30%';
+        
         document.getElementById('p2').style.setProperty('--mouseX',-50 +"%");
         document.getElementById('p2').style.setProperty('--mouseY',-30+"%");
 
-
-        document.getElementById('p3').style.top='-05%';
-        document.getElementById('p3').style.left='20%';
         document.getElementById('p3').style.setProperty('--mouseX',30 +"%");
         document.getElementById('p3').style.setProperty('--mouseY',30+"%");
       
-       
-        
-        document.getElementById('p4').style.top='35%';
-        document.getElementById('p4').style.left='30%';
         document.getElementById('p4').style.setProperty('--mouseX',30 +"%");
         document.getElementById('p4').style.setProperty('--mouseY',30+"%");
 
-        document.getElementById('p5').style.top='65%';
-        document.getElementById('p5').style.left='0%';
-        document.getElementById('p5').style.setProperty('--mouseX',30 +"%");
-        document.getElementById('p5').style.setProperty('--mouseY',30+"%");
+        document.getElementById('p5').style.setProperty('--mouseX',-80 +"%");
+        document.getElementById('p5').style.setProperty('--mouseY',60+"%");
 
-
-        document.getElementById('p6').style.top='55%';
-        document.getElementById('p6').style.left='-10%';
-        document.getElementById('p6').style.setProperty('--mouseX',30 +"%");
-        document.getElementById('p6').style.setProperty('--mouseY',30+"%");
+        document.getElementById('p6').style.setProperty('--mouseX',-30 +"%");
+        document.getElementById('p6').style.setProperty('--mouseY',-30+"%");
 }
 let scrolltop = 0;
 let scalee = 0;
